e9d75c25fe1ac4560c80d4796508ed2362b01dcb
julia <payuschikj@edi-n.com>
2020-06-09 20:50:13 +0300

    add less1

--- src/main/java/binarygaps/Solution.java
+++ src/main/java/binarygaps/Solution.java
@@ -1,4 +1,39 @@
 package binarygaps;
 
 public class Solution {
+
+    public int solution(int N) {
+        final String binaryStr = Integer.toBinaryString(N);
+
+        int max = 0;
+        int count = 0;
+        boolean isOpen = false;
+        boolean isClose = false;
+        for (char ch: binaryStr.toCharArray()) {
+            if (ch == '1') {
+                if (isOpen) {
+                    isOpen = false;
+                    isClose = true;
+                    if (max < count) {
+                        max = count;
+                    }
+                    count = 0;
+                    continue;
+                } else {
+                    isOpen = true;
+                    continue;
+                }
+            } else {
+                //ch=0
+                count += 1;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.solution(529);
+        System.out.println(result);
+    }
 }
