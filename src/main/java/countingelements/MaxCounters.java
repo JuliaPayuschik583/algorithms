9def515aab05e8d21f14ab704ddca2a3b19790d7
julia <payuschikj@edi-n.com>
2020-06-30 16:54:53 +0300

    Counting Elements (lesson 4): add MaxCounters algorithm

--- src/main/java/countingelements/MaxCounters.java
+++ src/main/java/countingelements/MaxCounters.java
@@ -3,6 +3,39 @@ package countingelements;
 public class MaxCounters {
 
     public int[] solution(int N, int[] A) {
+        final int condition = N + 1;
+        int currentMax = 0;
+        int lastUpdate = 0;
+        int countersArray[] = new int[N];
+
+        for (int iii = 0; iii < A.length; iii++) {
+            int currentValue = A[iii];
+            if (currentValue == condition) {
+                lastUpdate = currentMax;
+            } else {
+                int position = currentValue - 1;
+                if (countersArray[position] < lastUpdate) {
+                    countersArray[position] = lastUpdate + 1;
+                } else {
+                    countersArray[position]++;
+                }
+
+                if (countersArray[position] > currentMax) {
+                    currentMax = countersArray[position];
+                }
+            }
+
+        }
+
+        for (int iii = 0; iii < N; iii++) {
+            if (countersArray[iii] < lastUpdate)
+                countersArray[iii] = lastUpdate;
+        }
+
+        return countersArray;
+    }
+
+    public int[] solution2(int N, int[] A) {
         int[] zero = new int[N];
 
         int max = 0;
@@ -26,9 +59,9 @@ public class MaxCounters {
     public static void main(String[] args) {
         int N = 5;
         int[] A = new int[7];
-        A[0] = 3;
-        A[1] = 4;
-        A[2] = 4;
+        A[0] = 6;
+        A[1] = 3;
+        A[2] = 1;
         A[3] = 6;
         A[4] = 1;
         A[5] = 4;
